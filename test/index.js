90595392386775ab461ceb5eb7e916e56dccb6b2
Tim Branyen <tim@tabdeveloper.com>
2015-03-19 18:18:37 -0700

    Clean up index

--- test/index.js
+++ test/index.js
@@ -5,18 +5,14 @@ var bin = "./node_modules/.bin/istanbul";
 var cov = "cover --report=lcov _mocha --".split(" ");
 
 if (process.platform === 'win32') {
-  bin = "./node_modules/.bin/mocha";
+  bin = "./node_modules/mocha/bin/mocha";
   cov = [];
 }
 
 var args = cov.concat([
-  "test/runner.js",
+  "test/runner",
   "test/tests",
   "--expose-gc"
 ]);
 
-fork(bin, args, {
-  cwd: path.join(__dirname, "../")
-}).on("close", function(code) {
-  process.exit(code);
-});
+fork(bin, args, { cwd: path.join(__dirname, "../") }).on("close", process.exit);
